7e00f31fa26b01ca4592caaa83598c79c89ee321
BangYuWen <bangyuwen@gmail.com>
2017-11-10 20:15:20 +0800

    no message

--- index.js
+++ index.js
@@ -17,32 +17,32 @@ app.post('/webhook', line.middleware(lineClient.config), (req, res) => {
 function handleEvent(event) {
 
   console.log(event);
-  //
-  // if (event.type !== 'message' || event.message.type !== 'text') {
-  //   // ignore non-text-message event
-  //   return Promise.resolve(null);
-  // }
-  //
-  // let reply = '';
-  // let text = event.message.text;
-  // if (text[0] === '#') {
-  //   let parsedText = text.split(' ');
-  //   let command = parsedText[0].slice(1);
-  //   switch (command) {
-  //     case '菜單':
-  //       reply = submitMenu(parsedText[1]);
-  //       break;
-  //     default:
-  //       return;
-  //   }
-  //
-  //
-  //   // create a echoing text message
-  //   const echo = { type: 'text', text: reply };
-  //
-  //   // use reply API
-  //   return client.replyMessage(event.replyToken, echo);
-  // }
+
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  let reply = '';
+  let text = event.message.text;
+  if (text[0] === '#') {
+    let parsedText = text.split(' ');
+    let command = parsedText[0].slice(1);
+    switch (command) {
+      case '菜單':
+        reply = submitMenu(parsedText[1]);
+        break;
+      default:
+        return;
+    }
+
+
+    // create a echoing text message
+    const echo = { type: 'text', text: reply };
+
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+  }
 
   return Promise.resolve(null);
 
