d6bf931df3fe2111972647e55de2ddf800657477
BangYuWen <bangyuwen@gmail.com>
2017-11-10 21:14:18 +0800

    no message

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ function handleEvent(event) {
       case '山泉水':
         reply = {
             "type": "image",
-            "originalContentUrl": "./public/菜單_山泉水.jpg",
-            "previewImageUrl": "./public/菜單_山泉水.jpg"
+            "originalContentUrl": "./菜單_山泉水.jpg",
+            "previewImageUrl": "./菜單_山泉水.jpg"
         }
         break;
       case '乖孫':
