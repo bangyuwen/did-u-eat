bffc015e9812c81f90f08c8679e91f4be82734ff
BangYuWen <bangyuwen@gmail.com>
2017-11-10 11:43:46 +0800

    sdk

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+'use strict';
+
 const line = require('@line/bot-sdk');
 const express = require('express');
 
+// create LINE SDK config from env variables
 const config = {
-  channelSecret: '9ab2152f233a8cb7f11f4b901b7796e8',
-  channelAccessToken: '9G2nl534pVMJVQGrAyi3Qnuoxuj9LahTFgXX3CWo/D9cQvZgRMKW/06jb3ybK14mibNCJtQg/I+VWdTyrOqJdhtHSL5lH8/415ff6kd21T9yiRKqohCQDfnTOdJ1sr9Tj0J8QUCzD8J5SwutLcb3fAdB04t89/1O/w1cDnyilFU='
+  channelAccessToken: '9G2nl534pVMJVQGrAyi3Qnuoxuj9LahTFgXX3CWo/D9cQvZgRMKW/06jb3ybK14mibNCJtQg/I+VWdTyrOqJdhtHSL5lH8/415ff6kd21T9yiRKqohCQDfnTOdJ1sr9Tj0J8QUCzD8J5SwutLcb3fAdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '9ab2152f233a8cb7f11f4b901b7796e8'
 };
 
 // create LINE SDK client
