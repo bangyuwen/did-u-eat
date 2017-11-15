aff856161b07d9891eaac5da4163d9d3dd2a9843
BangYuWen <bangyuwen@gmail.com>
2017-11-15 19:39:02 +0800

    no message

--- src/menu.js
+++ src/menu.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const lineClient = require('../lineClient.js');
 const firebaseClient = require('../firebaseClient');
 
-module.exports = (shopName, messageId) => {
+exports.storeMenu = (shopName, messageId) => {
+  console.log('hi');
   const imgId = new Date().getTime();
   const stream = lineClient.getMessageContent(messageId);
 
   stream.pipe(fs.createWriteStream(`./public/${imgId}.jpg`));
-  firebaseClient.database().ref(`menus/ ${shopName}`).set({
+  firebaseClient.database().ref(`menus/ ${imgId}`).set({
     Name: shopName,
     id: imgId,
   }).catch((error) => {
